8699037c6973cd5e4a788e552fd4bfa83f1f20e2
insaecula <morozzzik228@gmail.com>
2025-03-04 13:23:27 +0300

    c

--- skyshop/src/main/java/org/skypro/skyshop/service/SearchService.java
+++ skyshop/src/main/java/org/skypro/skyshop/service/SearchService.java
@@ -1,10 +1,14 @@
 package org.skypro.skyshop.service;
-
+import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.search.SearchResult;
-
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,10 +20,21 @@ public class SearchService {
         this.storageService = storageService;
     }
 
-    public Collection<SearchResult> search(String pattern) {
+
+
+    public Collection<Object> search(String query) {
         return storageService.getAllSearchable().stream()
-                .filter(searchable -> searchable.getSearchTerm().toLowerCase().contains(pattern.toLowerCase()))
-                .map(SearchResult::fromSearchable)
+                .filter(searchable -> {
+                    if (searchable instanceof Article) {
+                        Article article = (Article) searchable;
+                        return article.getSearchTerm().toLowerCase().contains(query.toLowerCase());
+                    } else if (searchable instanceof Product) {
+                        Product product = (Product) searchable;
+                        return product.getSearchTerm().toLowerCase().contains(query.toLowerCase());
+                    }
+                    return false;
+                })
                 .collect(Collectors.toList());
+
     }
 }
