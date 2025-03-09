c4408d85ec7984d2debc48d7da8b57773a7abf3f
insaecula <morozzzik228@gmail.com>
2025-03-09 13:51:42 +0300

    c

---
+++ skyshop/src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -0,0 +1,23 @@
+package org.skypro.skyshop.model.basket;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+    public class ProductBasket implements Serializable {
+
+        private final Map<UUID, Integer> products = new HashMap<>();
+
+
+        public void addProduct(UUID id) {
+            products.put(id, products.getOrDefault(id, 0) + 1);
+        }
+
+
+        public Map<UUID, Integer> getProducts() {
+            return Collections.unmodifiableMap(products);
+        }
+    }
+
